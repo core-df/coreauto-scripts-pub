8e22261ff461ae9db7749433ae7e792b4f905cc4
MarekF <marek.frydryszak3@orange.com>
2026-05-27 19:35:26 +0200

    initial

---
+++ cawbs.py
@@ -0,0 +1,102 @@
+import os
+import requests
+import json
+
+wbs_iniflag=False
+wbs_env=os.environ.get('ENV')
+wbs_actionid=os.environ.get('ACTIONID')
+wbs_accesscode=os.environ.get('CA_ACCESS_CODE')
+wbs_url=os.environ.get('CA_WBS_URL')
+wbs_step=os.environ.get('STEPNAME')
+wbs_headers={}
+
+def Init():
+  global wbs_headers, wbs_iniflag, wbs_url
+  if wbs_env==None or wbs_actionid==None or wbs_accesscode==None or wbs_url==None or wbs_step==None:
+    return { 'status_code':601, 'error':'Environment variables ENV, ACTIONID, CA_ACCESS_CODE, CA_WBS_URL, STEPNAME should be defined' }
+  elif wbs_iniflag  :
+    return { 'status_code':602, 'error':'init already called' }
+  wbs_url=wbs_url.strip('/ ')
+  todo = { "apiCode": wbs_accesscode }
+  wbs_headers =  {"Content-Type":"application/json", "Environment":wbs_env}
+  response = requests.post(wbs_url + '/v1/auth/apicode', data=json.dumps(todo), headers=wbs_headers)
+
+  if response.status_code >= 400:
+    try:
+      js = response.json()
+    except :
+      return {'status_code': response.status_code, 'error': 'inaccessible'}
+    return {'status_code': response.status_code, 'error': js}
+
+  js = response.json()
+  wbs_headers["Authorization"]= 'Bearer ' + js["token"]
+  wbs_iniflag=True
+  return { 'status_code': response.status_code }
+
+
+def GetEventPayload():
+  if not wbs_iniflag :
+     return { 'status_code':603, 'error':'Init required' }
+  response = requests.get(wbs_url + '/v1/rtevent/' + wbs_actionid, headers=wbs_headers)
+
+  if response.status_code >= 400:
+    try:
+      js = response.json()
+    except :
+      return {'status_code': response.status_code, 'error': 'inaccessible'}
+    return {'status_code': response.status_code, 'error': js}
+
+  js=response.json()
+  return { 'status_code': response.status_code, 'payload':js["payload"] }
+
+
+def PutStepPayload(payload):
+  if not wbs_iniflag :
+     return { 'status_code':603, 'error':'Init required' }
+  todo = { "actionId": wbs_actionid, "stepname": wbs_step, "payload":  payload }
+  response = requests.post(wbs_url + '/v1/rtstep/payload', data=json.dumps(todo), headers=wbs_headers)
+
+  if response.status_code >= 400:
+    try:
+      js = response.json()
+    except :
+      return {'status_code': response.status_code, 'error': 'inaccessible'}
+    return {'status_code': response.status_code, 'error': js}
+
+  return { 'status_code': response.status_code }
+
+
+def GetStepPayload(stepname):
+  if not wbs_iniflag :
+     return { 'status_code':603, 'error':'Init required' }
+  response = requests.get(wbs_url + '/v1/rtstep/payload/' + wbs_actionid + "/" + stepname, headers=wbs_headers)
+
+  if response.status_code >= 400:
+    try:
+      js = response.json()
+    except :
+      return {'status_code': response.status_code, 'error': 'inaccessible'}
+    return {'status_code': response.status_code, 'error': js}
+
+  js=response.json()
+  return { 'status_code': response.status_code, 'payload':js["payload"] }
+
+
+def GetKeystore(keylist):
+  if not wbs_iniflag :
+     return { 'status_code':603, 'error':'Init required' }
+  keys=keylist.replace(' ','')
+  response = requests.get(wbs_url + '/v1/keystore/' + keys, headers=wbs_headers)
+
+  if response.status_code >= 400:
+    try:
+      js = response.json()
+    except :
+      return {'status_code': response.status_code, 'error': 'inaccessible'}
+    return {'status_code': response.status_code, 'error': js}
+
+  js=response.json()
+  for key in keys.split(','):
+    if not key in js:
+      return { 'status_code': 605, 'error': key + ' not found' }
+  return { 'status_code': response.status_code, 'answer':js }
